1a893d1b4817f9c0417ac4116bde5b1ddefff53b
TheMaxwellJames <maxichanj@gmail.com>
2024-02-27 19:30:43 +0100

    completed multi authentication for admin and user

---
+++ resources/views/admin/body/sidebar.blade.php
@@ -0,0 +1,59 @@
+<nav class="sidebar">
+    <div class="sidebar-header">
+      <a href="#" class="sidebar-brand">
+        Real<span>Estate</span>
+      </a>
+      <div class="sidebar-toggler not-active">
+
+      </div>
+    </div>
+    <div class="sidebar-body">
+      <ul class="nav">
+
+        <li class="nav-item">
+          <a href="{{route('admin.dashboard')}}" class="nav-link">
+            <i class="link-icon" data-feather="box"></i>
+            <span class="link-title">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item nav-category">Real Estate</li>
+        <li class="nav-item">
+          <a class="nav-link" data-bs-toggle="collapse" href="#emails" role="button" aria-expanded="false" aria-controls="">
+
+            <span class="link-title">Properties</span>
+            <i class="link-arrow" data-feather="chevron-down"></i>
+          </a>
+          <div class="collapse" id="emails">
+            <ul class="nav sub-menu">
+              <li class="nav-item">
+                <a href="" class="nav-link">All Property</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Add Propery</a>
+              </li>
+
+            </ul>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <a href="pages/apps/calendar.html" class="nav-link">
+
+            <span class="link-title">Chat</span>
+          </a>
+        </li>
+
+
+
+
+
+
+
+
+
+
+
+
+      </ul>
+    </div>
+  </nav>
