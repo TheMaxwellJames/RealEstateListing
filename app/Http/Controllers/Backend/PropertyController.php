8121d085a9fb8faf0215374d761da15d20d286a8
TheMaxwellJames <maxichanj@gmail.com>
2024-03-02 17:20:38 +0100

    completed user

--- app/Http/Controllers/Backend/PropertyController.php
+++ app/Http/Controllers/Backend/PropertyController.php
@@ -9,19 +9,22 @@ use App\Http\Controllers\Controller;
 
 class PropertyController extends Controller
 {
-    public function index()
-    {
-
-        $properties = Property::where('availability_date', '>=', date('Y-m-d'))->get();
-
-        return view('properties.index', compact('properties'));
-    }
-
     
 
-    public function show(Property $property)
-    {
 
-        return view('properties.show', compact('property'));
-    }
+    public function index()
+{
+    $properties = Property::all();
+
+    return view('frontend.index',  compact('properties'));
+}
+
+
+
+
+    // public function show(Property $property)
+    // {
+
+    //     return view('properties.show', compact('property'));
+    // }
 }
